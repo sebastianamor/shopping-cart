cff8f04e68ff07c32b2bbb497c52c181e9cbb2a3
¨Jane <smoreira11@hotmail.com>
2024-09-05 22:50:44 +0900

    今から日本語

--- script.js
+++ script.js
@@ -11,90 +11,91 @@ const showHideCartSpan = document.getElementById("show-hide-cart");
 let isCartShowing = false;
 
 const products = [
-  {
-    id: 1,
-    name: "Vanilla Cupcakes (6 Pack)",
-    price: 12.99,
-    category: "Cupcake",
+  
+    {
+      id: 1,
+      name: "バニラカップケーキ（6個入り）",
+      price: 12.99,
+      category: "カップケーキ",
   },
   {
-    id: 2,
-    name: "French Macaron",
-    price: 3.99,
-    category: "Macaron",
+      id: 2,
+      name: "フレンチマカロン",
+      price: 3.99,
+      category: "マカロン",
   },
   {
-    id: 3,
-    name: "Pumpkin Cupcake",
-    price: 3.99,
-    category: "Cupcake",
+      id: 3,
+      name: "パンプキンカップケーキ",
+      price: 3.99,
+      category: "カップケーキ",
   },
   {
-    id: 4,
-    name: "Chocolate Cupcake",
-    price: 5.99,
-    category: "Cupcake",
+      id: 4,
+      name: "チョコレートカップケーキ",
+      price: 5.99,
+      category: "カップケーキ",
   },
   {
-    id: 5,
-    name: "Chocolate Pretzels (4 Pack)",
-    price: 10.99,
-    category: "Pretzel",
+      id: 5,
+      name: "チョコレートプレッツェル（4個入り）",
+      price: 10.99,
+      category: "プレッツェル",
   },
   {
-    id: 6,
-    name: "Strawberry Ice Cream",
-    price: 2.99,
-    category: "Ice Cream",
+      id: 6,
+      name: "ストロベリーアイスクリーム",
+      price: 2.99,
+      category: "アイスクリーム",
   },
   {
-    id: 7,
-    name: "Chocolate Macarons (4 Pack)",
-    price: 9.99,
-    category: "Macaron",
+      id: 7,
+      name: "チョコレートマカロン（4個入り）",
+      price: 9.99,
+      category: "マカロン",
   },
   {
-    id: 8,
-    name: "Strawberry Pretzel",
-    price: 4.99,
-    category: "Pretzel",
+      id: 8,
+      name: "ストロベリープレッツェル",
+      price: 4.99,
+      category: "プレッツェル",
   },
   {
-    id: 9,
-    name: "Butter Pecan Ice Cream",
-    price: 2.99,
-    category: "Ice Cream",
+      id: 9,
+      name: "バターペカンアイスクリーム",
+      price: 2.99,
+      category: "アイスクリーム",
   },
   {
-    id: 10,
-    name: "Rocky Road Ice Cream",
-    price: 2.99,
-    category: "Ice Cream",
+      id: 10,
+      name: "ロッキーロードアイスクリーム",
+      price: 2.99,
+      category: "アイスクリーム",
   },
   {
-    id: 11,
-    name: "Vanilla Macarons (5 Pack)",
-    price: 11.99,
-    category: "Macaron",
+      id: 11,
+      name: "バニラマカロン（5個入り）",
+      price: 11.99,
+      category: "マカロン",
   },
   {
-    id: 12,
-    name: "Lemon Cupcakes (4 Pack)",
-    price: 12.99,
-    category: "Cupcake",
+      id: 12,
+      name: "レモンカップケーキ（4個入り）",
+      price: 12.99,
+      category: "カップケーキ",
   },
-];
+]
 
 products.forEach(
   ({ name, id, price, category }) => {
     dessertCards.innerHTML += `
       <div class="dessert-card">
         <h2>${name}</h2>
-        <p class="dessert-price">$${price}</p>
-        <p class="product-category">Category: ${category}</p>
+        <p class="dessert-price">¥${price}</p>
+        <p class="product-category">カテゴリ： ${category}</p>
         <button 
           id="${id}" 
-          class="btn add-to-cart-btn">Add to cart
+          class="btn add-to-cart-btn"> カートに追加
         </button>
       </div>
     `;
@@ -105,7 +106,7 @@ class ShoppingCart {
   constructor() {
     this.items = [];
     this.total = 0;
-    this.taxRate = 8.25;
+    this.taxRate = 10;
   }
 
   addItem(id, products) {
@@ -132,7 +133,62 @@ class ShoppingCart {
       </div>
       `;
   }
-
+  
   getCounts() {
     return this.items.length;
-  }}
+  }
+  clearCart() {
+    if (!this.items.length) {
+      alert("ショッピングカートはすでに空です");
+      return;
+    }
+  
+    const isCartCleared = confirm(
+      "ショッピングカートのすべてのアイテムをクリアしてもよろしいですか？"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `¥${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `¥${tax.toFixed(2)}`;
+    cartTotal.textContent = `¥${this.total.toFixed(2)}`;
+    return this.total;
+  }
+};
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+      cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
+    })
+  }
+);
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+  showHideCartSpan.textContent = isCartShowing ? "非表示" : "表示";
+  cartContainer.style.display = isCartShowing ? "block" : "none";
+});
+clearCartBtn.addEventListener("click",cart.clearCart.bind(cart) );
